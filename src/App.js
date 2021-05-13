7ea380fbcebcf395101f57c508ce7471ab071d07
Destiny Wu <wudestin@amazon.com>
2021-05-13 14:11:47 -0700

    Revert

--- src/App.js
+++ src/App.js
@@ -13,13 +13,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
-          <Game/>
-=======
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
->>>>>>> parent of d9dbe3c... Implemented ListTodos
           <a
             className="App-link"
             href="https://reactjs.org"
