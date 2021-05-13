34b819f575e5cae3fedd18a68f36f061f9685882
Destiny Wu <wudestin@amazon.com>
2021-05-13 14:39:09 -0700

    Revert

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import Game from '.Game';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
