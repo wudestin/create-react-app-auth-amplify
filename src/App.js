1785fa34f7f619811e216b87c7382b10f76a2a20
Destiny Wu <wudestin@amazon.com>
2021-05-13 14:25:17 -0700

    Invoke Game

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <Game/>
           <a
             className="App-link"
             href="https://reactjs.org"
