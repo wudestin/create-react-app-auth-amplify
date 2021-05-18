92cdfe42ce84f0174eaae14caf77e3ab01c75098
Destiny Wu <wudestin@amazon.com>
2021-05-18 09:55:12 -0700

    Added drag-and-drop list

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <List/>
           <Game/>
           <a
             className="App-link"
